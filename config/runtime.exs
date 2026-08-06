894c39d4c59677d0e701d30f342c475d2cce1e3f
Frank Burnham <frank@n8git.dev>
2026-08-06 16:12:31 -0400

    Config from env

--- config/runtime.exs
+++ config/runtime.exs
@@ -10,27 +10,35 @@ alias Weaver.Api.Ollama
 
 config :weaver,
   llm: %LLM{
-    api: Weaver.Api.Bedrock
-    # api: Weaver.Api.OllamaMock
-    # api: Weaver.Api.Ollama,
+    # api: Weaver.Api.Bedrock
+    api: Weaver.Api.OllamaMock
+    # api: Weaver.Api.Ollama
     # api: Weaver.Api.BedrockMock
   },
   history: %History{
-    base_dir: ".weaver/history/"
+    base_dir: System.get_env("WEAVER_HISTORY_BASE_DIR", ".weaver/history/")
   },
   tools: %Tools{
-    base_dir: "~/.agent.nu/tools/"
+    base_dir: System.get_env("WEAVER_TOOLS_BASE_DIR")
   },
   tui: %TUI{
-    show_thinking: false
+    show_thinking:
+      if System.get_env("WEAVER_SHOW_THINKING", "no") == "yes" do
+        true
+      else
+        false
+      end
   },
   personas: %Personas{
-    base_dir: "~/.agent.nu/bedrock-personas/",
-    name: "glm5-coding-agent"
+    base_dir: System.get_env("WEAVER_PERSONAS_BASE_DIR"),
+    name: System.get_env("WEAVER_PERSONA")
   },
   bedrock: %Request{
     credential_process: &Weaver.Api.Bedrock.Request.awscli_credential_process/0
   },
   ollama: %Ollama{
-    base_url: "http://example.com/"
+    base_url: System.get_env("WEAVER_OLLAMA_BASE_URL")
   }
+
+# TODO: optionally pull config details from XDG location?
+# TODO: fail fast if a value is missing
